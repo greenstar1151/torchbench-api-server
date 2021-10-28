fc8dc735dc4e5d3d5ee24f6fa15f0d45528ae0d1
Biswadip Maity <biswadip.maity@gmail.com>
2021-10-27 20:21:09 -0700

    Cache only 1 model

--- app/main.py
+++ app/main.py
@@ -14,9 +14,11 @@ sys.path.append(os.path.join(MAIN_DIR.parent.absolute(), 'pytorch_benchmark'))
 from pytorch_benchmark.torchbenchmark import list_models
 
 # This is used to cache already queried models in PyTorch
+lastLoaded=''
 loaded = {}
 app = FastAPI()
 nvMetrics=NvidiaMetrics()
+print("[YOLO] calling main!")
 
 @app.get('/')
 async def read_root():
@@ -33,13 +35,18 @@ async def list():
 
 @app.get('/eval')
 def run_model(model: str, niter: int = 1, device: str = 'cuda', mode: str = 'jit', test: str = 'eval'):
+    global lastLoaded
+    global loaded
+
     found = False
     for Model in list_models():
         if model.lower() in Model.name.lower():
             found = True
             key=model+'_'+device
-            if key not in loaded:
-                loaded[key] = Model(device=device, jit=(mode == 'jit'))
+            if key != lastLoaded:
+                lastLoaded=key
+                del loaded
+                loaded = Model(device=device, jit=(mode == 'jit'))
             break
     if found:
         pass
@@ -48,7 +55,8 @@ def run_model(model: str, niter: int = 1, device: str = 'cuda', mode: str = 'jit
         raise HTTPException(status_code=404, detail=f"Unable to find model matching '{model}'.")
     
     # build the model and get the chosen test method
-    test = getattr(loaded[key], test)
+    print(loaded)
+    test = getattr(loaded, test)
 
     time_cpu_total_wall, time_cpu_dispatch, time_gpu = helper.run_one_step(test, device == 'cuda', niter)
 
@@ -59,7 +67,7 @@ def run_model(model: str, niter: int = 1, device: str = 'cuda', mode: str = 'jit
             'mode': mode,
             'task': test,
             'niter': niter,
-            'size': helper.sizeof_fmt(helper.get_size(loaded[key]))
+            'size': helper.sizeof_fmt(helper.get_size(loaded))
         },
         'result': {
             'CPU Total Wall Time': time_cpu_total_wall, 
